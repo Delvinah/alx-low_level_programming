c2a2984ad0a32fd80451a9fc83fb0e3218f5f4e3
Delvin <delvinnyanganyi@gmail.com>
2023-06-16 08:35:45 -0700

    I sometimes suffer from insomnia

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char b;
+
+	b = 'e';
+	while
+		(b <= 'r') {
+			putchar(b);
+			b++;
+		}
+	putchar('\n');
+	return (0);
+}
