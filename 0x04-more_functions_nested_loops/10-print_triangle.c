0b3a65ce2d67a2bad24fc7034d0a256f8ffb73c6
Delvin <delvinnyanganyi@gmail.com>
2023-06-22 11:47:58 -0700

    Triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,7 +8,7 @@ void print_triangle(int size)
 {
 	if (size <= 0)
 	{
-		putchar('\n');
+		_putchar('\n');
 	}
 	else
 	{
@@ -18,13 +18,13 @@ void print_triangle(int size)
 		{
 			for (j = size - i; j > 0; j--)
 			{
-				putchar(' ');
+				_putchar(' ');
 			}
 			for (j = 1; j <= i; j++)
 			{
-				putchar('#');
+				_putchar('#');
 			}
-			putchar('\n');
+			_putchar('\n');
 		}
 	}
 }
