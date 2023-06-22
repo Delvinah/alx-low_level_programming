d7a5561b06474de27e551e25f0e417d01fe0f1c3
Delvin <delvinnyanganyi@gmail.com>
2023-06-22 07:09:29 -0700

    Numbers constitute the only universal language

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * more_numbers - prints ten times the number as from 0 to 14
+ * Return: returns no value
+ */
+void more_numbers(void)
+{
+	int x, y;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y <= 14; y++)
+		{
+			if (y > 9)
+			{
+				putchar((y / 10) + '0');
+			}
+			putchar((y % 10) + '0');
+		}
+		putchar('\n');
+	}
+}
