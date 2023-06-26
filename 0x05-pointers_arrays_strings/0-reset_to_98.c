ab09b56687b90e6642d78a58ab51eb352c08dfa9
Delvin <delvinnyanganyi@gmail.com>
2023-06-26 05:42:29 -0700

    98 Battery

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - function takes a pointer to an integer as a parameter
+ * updates the value it points to 98
+ * @n: input
+ * Return: no value
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
